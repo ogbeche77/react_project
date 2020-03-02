07fa9b4577e2ec1769ae8fb9b9ec2d00ffba9498
ogbeche77 <ogbeche77@yahoo.com>
2020-03-02 16:57:08 +0100

    uuid installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import about from './components/pages/about';
+import  {v4 as uuidv4} from 'uuid';
 
 import './App.css';
 
@@ -9,17 +12,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Make a doctor's appointment",
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Cook dinner",
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Learn more algorithms",
         completed: false
       }
@@ -41,17 +44,35 @@ class App extends Component {
   delTodo = (id)=> {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id!== id)] });
   }
+
+  //Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
   render(){ 
 return (
+  <Router>
   <div className="App">
-  
   <div className="container">
   <Header />
-  <AddTodo />
+  <Route exact path="/" render={props => (
+    <React.Fragment>
+    <AddTodo addTodo={this.addTodo} />
     <Todos todos={this.state.todos} markComplete={this.markComplete}
-  delTodo={this.delTodo}  />
-    </div>
+  delTodo={this.delTodo} />
+    </React.Fragment>
+  )} />
+<Route path='/about' component = {about}  />
+
+  </div>
   </div> 
+  </Router>
   
 );
 }
---
+++ src/components/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function about() {
+    return (
+      <React.Fragment>
+      <h1>About</h1>
+      <p>State of the art application to store all Todo activities. 
+      That way you never forget any activity or events</p>
+      </React.Fragment>
+    )
+}
+
+
+
+
+export default about;
