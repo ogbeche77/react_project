ddbb7c4af874b4061f1ab5a7885f23f4129ab518
ogbeche77 <ogbeche77@yahoo.com>
2020-03-17 18:50:45 +0100

    Updates

---
+++ precache-manifest.43d1cb29295d630806ec072886bf2e38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdded3d400aa7ec36a13e4c89c2aefe",
+    "url": "/react_project/index.html"
+  },
+  {
+    "revision": "a332304d75c345462941",
+    "url": "/react_project/static/css/main.61c1891b.chunk.css"
+  },
+  {
+    "revision": "5ea92a5b9bb72d65f718",
+    "url": "/react_project/static/js/2.2fd9c84c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react_project/static/js/2.2fd9c84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a332304d75c345462941",
+    "url": "/react_project/static/js/main.ef9cf476.chunk.js"
+  },
+  {
+    "revision": "38bcdbe53bd0ceaabad7",
+    "url": "/react_project/static/js/runtime-main.34ff0a55.js"
+  }
+]);
