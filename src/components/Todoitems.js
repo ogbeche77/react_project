36d1f88d739cf2c1579824879e7dbf308715e8df
ogbeche77 <ogbeche77@yahoo.com>
2020-03-17 09:40:57 +0100

    comments

--- src/components/Todoitems.js
+++ src/components/Todoitems.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; // prototype brough in & defined below
 
 
 
@@ -30,7 +30,7 @@ export class Todoitems extends Component {
 }
 
 
-//Proptypes
+//We define our Proptypes here
 Todoitems.propTypes = {
     todo: PropTypes.object.isRequired,
     markComplete: PropTypes.func.isRequired,
