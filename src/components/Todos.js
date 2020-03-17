3a667dd07c5b6cfb7f9fa8526337be36fa1d070d
ogbeche77 <ogbeche77@yahoo.com>
2020-03-17 09:35:39 +0100

    comments

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -9,7 +9,7 @@ markComplete = ()=>{
 
     render(){
           return this.props.todos.map((todo)=>( //map is used here to loop through & output jsx
-            <Todoitems key={todo.id} todo={todo} markComplete=
+            <Todoitems key={todo.id} todo={todo} markComplete= //Todoitems is outputed
             {this.props.markComplete} delTodo={this.props.delTodo} />
           ));
 }
